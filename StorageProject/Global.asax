78dc1d4f9fb38b3f45dd57d11aca82b7ad1a473e
filifionka15 <aleksandra33@inbox.ru>
2019-12-01 19:57:48 +0300

    Добавьте файлы проекта.

---
+++ StorageProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StorageProject.MvcApplication" Language="C#" %>
---
+++ StorageProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StorageProject.Startup))]
+namespace StorageProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
